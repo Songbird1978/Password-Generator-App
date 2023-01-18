a85cb0468bb3c09278e5d83e99defd396dedc899
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2023-01-18 13:58:35 +0000

    prompt and confirm messages working. getPassWordOptions function working. new array created from results of getPasswordOptions

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ var allCharactersObject = {
   ],
 
   // Array of numeric characters to be included in password
- numericCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  numericCharacters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
 
   // Array of lowercase characters to be included in password
   lowerCasedCharacters: [
@@ -94,42 +94,80 @@ var allCharactersObject = {
 
 };
 
+
+
+var specialCharTrue;
+var upperCharTrue;
+var lowCharTrue;
+var numbersTrue;
+
 let userData = [];
-/*
-class optionResults {
-  constructor (userLength, upperCharTrue, lowCharTrue, numbersTrue, specialCharTrue) {
-    this.
-  }
-} 
-*/
-
-
 
 
 // Function to prompt user for password options
 function getPasswordOptions() {
 
   var userInput = prompt("How long would you like your password to be?  Please input a number between 10 and 64!");
-
   var userLength = parseInt(userInput);
 
-  
-
   if (userLength >= 10 && userLength <= 64) {   
-    alert ( "Thankyou! Your password will be " + userLength + " characters long.") 
+    alert ( "Thankyou! Your password will be " + userLength + " characters long.");
+    userData.push(userLength);
+    
+    console.log (userLength);
   } else if (userLength < 10 || userLength > 64) {
     alert ( "You entered an incorrect number! Please enter a number between 10-64!" );
-  }
+  };
 
-  let addUserData = {
-    userLength: ,
-    upperCharTrue: ,
-    lowCharTrue: ,
-    numbersTrue:,
-    specialCharTrue: ;
-  }
+  var specialCharTrue = confirm ("Would you like your password to include Special characters?");
+    if (specialCharTrue === true ) { 
+      userData.push(specialCharTrue);
+      console.log (specialCharTrue);
+  } else if (specialCharTrue ===false) {
+    userData.push(specialCharTrue); 
+    console.log (specialCharTrue);
+};
+  
+  var upperCharTrue = confirm ( "Would you like your password to include uppercase characters?") 
+    if (upperCharTrue ===true )  {
+      userData.push(upperCharTrue);
+      console.log (upperCharTrue);   
+  } else if (upperCharTrue ===false) {
+      userData.push(upperCharTrue); 
+      console.log (upperCharTrue);
+  };
+  
+  var lowCharTrue = confirm ( "Would you like your password to include lowercase characters?") 
+  if (lowCharTrue ===true )  { 
+    userData.push(lowCharTrue);
+    console.log (lowCharTrue);
+  } else if (lowCharTrue ===false) {
+    userData.push(lowCharTrue); 
+    console.log (lowCharTrue);
+};
 
+  var numbersTrue = confirm ( "Would you like your password to include numerical characters?") 
+  if (numbersTrue ===true )  {
+    userData.push(numbersTrue);
+    console.log (numbersTrue);
+  } else if (numbersTrue ===false) {
+    userData.push(numbersTrue); 
+    console.log (numbersTrue);
+};
+  
 }
+
+getPasswordOptions (userData.push);
+console.log (userData);
+
+
+
+// Function for getting a random element from an object
+function getRandom() {
+  
+}
+
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
